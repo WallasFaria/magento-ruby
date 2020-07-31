c5b5213f2e56e52aeb34c8e60a32097d1a8318ef
Wallas Faria <wallasfaria@hotmail.com>
2020-07-31 05:42:28 -0300

    fix method name find_by_sku

--- lib/magento/product.rb
+++ lib/magento/product.rb
@@ -1,7 +1,7 @@
 module Magento
   class Product < Model
     class << self
-      def find_by_sky(sku)
+      def find_by_sku(sku)
         product_hash = Request.get("products/#{sku}").parse
         mapHash Product, product_hash
       end
