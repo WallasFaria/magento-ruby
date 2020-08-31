8f0b6293396037b2d1d6afd814bcb904b5cecdff
Wallas Faria <wallasfaria@hotmail.com>
2020-08-30 22:39:38 -0300

    fix: variable name on Invoice.refund

--- lib/magento/invoice.rb
+++ lib/magento/invoice.rb
@@ -92,7 +92,7 @@ module Magento
       #
       # or
       #
-      # Magento::Order.refund(
+      # Magento::Invoice.refund(
       #   invoice_id,
       #   items: [
       #     {
@@ -125,7 +125,7 @@ module Magento
       #
       # @return {Integer} return the refund id
       def refund(invoice_id, refund_params=nil)
-        request.post("invoice/#{order_id}/refund", invoice_params).parse
+        request.post("invoice/#{invoice_id}/refund", invoice_params).parse
       end
     end
   end
