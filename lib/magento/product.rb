ef39cd45410dfc78859082d7d3697743114adfbc
Wallas Faria <wallasfaria@hotmail.com>
2020-10-01 22:28:34 -0300

    allow get custom attribute value by custom attribute code in product
    Exemple:
    
    product.attr :description
    product.attr :image
    or
    product.description
    product.image
    
    when the custom attribute does not exists:
    product.attr :special_price
    > nil
    product.special_price
    > NoMethodError: undefined method `special_price' for #<Magento::Product:...>
    
    product.respond_to? :special_price
    > false
    
    product.respond_to? :description
    > true

--- lib/magento/product.rb
+++ lib/magento/product.rb
@@ -2,6 +2,20 @@ module Magento
   class Product < Model
     self.primary_key = :sku
 
+    def method_missing(m)
+      attr(m) || super
+    end
+
+    # returns custom_attribute value by custom_attribute code
+    # return nil if custom_attribute is not present
+    def attr(attribute_code)
+      @custom_attributes&.find { |a| a.attribute_code == attribute_code.to_s }&.value
+    end
+
+    def respond_to?(attribute_code)
+      super || @custom_attributes&.any? { |a| a.attribute_code == attribute_code.to_s }
+    end
+
     class << self
       alias_method :find_by_sku, :find
     end
