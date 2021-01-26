d22072bc892fba30a6a1ecdbf95248d26ffc8509
Wallas Faria <wallasfaria@hotmail.com>
2021-01-26 09:46:49 -0300

    test: Magento::Product.find

--- spec/magento/product_spec.rb
+++ spec/magento/product_spec.rb
@@ -1,4 +1,25 @@
 RSpec.describe Magento::Product do
+  let(:magento_client) { Magento::Request.new }
+
+  before { allow(Magento::Product).to receive(:request).and_return(magento_client) }
+
+  describe '.find' do
+    it 'request to /prducts/:sku' do
+      response = double('HTTP::Response', parse: {})
+
+      expect(magento_client).to receive(:get).with('products/1243').and_return(response)
+
+      Magento::Product.find('1243')
+    end
+
+    it 'returns a Magento::Product instance' do
+      VCR.use_cassette('product/find') do
+        product = Magento::Product.find('1243')
+        expect(product).to be_an_instance_of(Magento::Product)
+      end
+    end
+  end
+
   describe '#set_custom_attribute' do
     let(:product) { Magento::Product.build(
       sku: 25,
