d242ad14ef235596b656f2fc40a87b41df26755d
Wallas Faria <wallasfaria@hotmail.com>
2021-01-26 09:16:28 -0300

    chore: config vcr

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in magento.gemspec
 gemspec
 
-gem "rake", "~> 12.0"
-gem "rspec", "~> 3.0"
 gem 'byebug', '~> 11.1', '>= 11.1.3'
+gem 'rake', '~> 12.0'
+gem 'rspec', '~> 3.0'
+gem 'vcr'
+gem 'webmock'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
-require "bundler/setup"
+require 'bundler/setup'
 require 'byebug'
-require "magento"
+require 'magento'
+require 'vcr'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
@@ -12,4 +13,13 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.filter_sensitive_data('<MAGENTO_URL>') { ENV['MAGENTO_URL'] }
+  c.filter_sensitive_data('<MAGENTO_DOMAIN>') { ENV['MAGENTO_URL'].to_s.sub(/^http(s)?:\/\//, '') }
+  c.filter_sensitive_data('<MAGENTO_TOKEN>') { ENV['MAGENTO_TOKEN'] }
 end
