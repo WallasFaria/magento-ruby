38fa78696c6e08a4cc588ab267745fe4601a2b6f
Wallas Faria <wallasfaria@hotmail.com>
2021-01-28 00:04:53 -0300

    test: describe pending specs to Magento::ModelMapper

---
+++ spec/magento/core/model_mapper_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe Magento::ModelMapper do
+  describe '.to_hash' do
+    it 'is pending'
+  end
+
+  describe '.map_hash' do
+    it 'is pending'
+  end
+
+  describe '.map_array' do
+    it 'is pending'
+  end
+
+  describe 'ModelParser.build' do
+    it 'is pending'
+  end
+
+  describe 'ModelParser#to_h' do
+    it 'is pending'
+  end
+end
