257a4e41043bcf469d3b42695a4179afbc1ca493
Wallas Faria <wallasfaria@hotmail.com>
2021-01-27 23:55:14 -0300

    test: describe pending specs to Magento::Model

---
+++ spec/magento/core/model_spec.rb
@@ -0,0 +1,71 @@
+RSpec.describe Magento::Model do
+  describe 'public method' do
+    describe '.find' do
+      it 'is pending'
+    end
+
+    describe '.create' do
+      it 'is pending'
+    end
+
+    describe '.update' do
+      it 'is pending'
+    end
+
+    describe '.delete' do
+      it 'is pending'
+    end
+
+    describe '#save' do
+      it 'calls the update class method'
+    end
+
+    describe '#update' do
+      it 'calls the update class method'
+    end
+
+    describe '#delete' do
+      it 'calls the delete class method'
+    end
+
+    describe '.api_resource' do
+      it 'is pending'
+    end
+
+    describe '.entity_name' do
+      it 'is pending'
+    end
+
+    describe '.primary_key' do
+      it 'is pending'
+    end
+
+    describe 'delegated methods from query' do
+      it 'respond to .all'
+      it 'respond to .find_each'
+      it 'respond to .page'
+      it 'respond to .per'
+      it 'respond to .page_size'
+      it 'respond to .order'
+      it 'respond to .select'
+      it 'respond to .where'
+      it 'respond to .first'
+      it 'respond to .find_by'
+      it 'respond to .count'
+    end
+  end
+
+  describe 'protected method' do
+    describe '.entity_key' do
+      it 'is pending'
+    end
+
+    describe '.query' do
+      it 'is pending'
+    end
+
+    describe '.request' do
+      it 'is pending'
+    end
+  end
+end
