40d0df76602ee8462536d1698032ce54797e7654
Wallas Faria <wallasfaria@hotmail.com>
2021-01-28 00:12:52 -0300

    test: describe pending specs to Magento::Query

---
+++ spec/magento/core/query_spec.rb
@@ -0,0 +1,75 @@
+RSpec.describe Magento::Query do
+  describe '#where' do
+    it 'is pending'
+  end
+
+  describe '#page' do
+    it 'is pending'
+  end
+
+  describe '#page_size' do
+    it 'is pending'
+  end
+
+  describe '#select' do
+    it 'is pending'
+  end
+
+  describe '#order' do
+    it 'is pending'
+  end
+
+  describe '#all' do
+    it 'is pending'
+  end
+
+  describe '#first' do
+    it 'is pending'
+  end
+
+  describe '#find_by' do
+    it 'is pending'
+  end
+
+  describe '#count' do
+    it 'is pending'
+  end
+
+  describe '#find_each' do
+    it 'is pending'
+  end
+
+  describe 'private mathods' do
+    describe 'endpoint' do
+      it 'is pending'
+    end
+
+    describe 'verify_id' do
+      it 'is pending'
+    end
+
+    describe 'query_params' do
+      it 'is pending'
+    end
+
+    describe 'parse_filter' do
+      it 'is pending'
+    end
+
+    describe 'parse_value_filter' do
+      it 'is pending'
+    end
+
+    describe 'parse_field' do
+      it 'is pending'
+    end
+
+    describe 'encode' do
+      it 'is pending'
+    end
+
+    describe 'append_key' do
+      it 'is pending'
+    end
+  end
+end
