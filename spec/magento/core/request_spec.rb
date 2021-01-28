46fbaa6cb41cf6bfb9e96740d3920e0f3462bb49
Wallas Faria <wallasfaria@hotmail.com>
2021-01-28 00:16:28 -0300

    test: describe pending specs to Magento::Request

---
+++ spec/magento/core/request_spec.rb
@@ -0,0 +1,43 @@
+RSpec.describe Magento::Request do
+  describe '#where' do
+    it 'is pending'
+  end
+
+  describe '#get' do
+    it 'is pending'
+  end
+
+  describe '#put' do
+    it 'is pending'
+  end
+
+  describe '#post' do
+    it 'is pending'
+  end
+
+  describe '#delete' do
+    it 'is pending'
+  end
+
+  context 'private method' do
+    describe '#http_auth' do
+      it 'is pending'
+    end
+
+    describe '#base_url' do
+      it 'is pending'
+    end
+
+    describe '#url' do
+      it 'is pending'
+    end
+
+    describe '#handle_error' do
+      it 'is pending'
+    end
+
+    describe '#save_request' do
+      it 'is pending'
+    end
+  end
+end
