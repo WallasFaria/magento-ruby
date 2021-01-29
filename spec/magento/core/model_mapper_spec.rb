e7def3c4c4b05c8f7f1d593d1b4dfb45d4aae8c1
Wallas Faria <wallasfaria@hotmail.com>
2021-01-28 21:00:37 -0300

    test: Magento::ModelMapper

--- spec/magento/core/model_mapper_spec.rb
+++ spec/magento/core/model_mapper_spec.rb
@@ -1,21 +1,74 @@
 RSpec.describe Magento::ModelMapper do
   describe '.to_hash' do
-    it 'is pending'
+    it 'serializes object to hash' do
+      class Magento::SameClass
+        attr_accessor :name, :description, :items
+      end
+
+      object = Magento::SameClass.new
+      object.name = 'Some name'
+      object.description = 'Some description'
+      object.items = [object.dup]
+
+      expect(Magento::ModelMapper.to_hash(object)).to eql({
+        'name' => 'Some name',
+        'description' => 'Some description',
+        'items' => [{ 'name' => 'Some name', 'description' => 'Some description' }]
+      })
+    end
   end
 
   describe '.map_hash' do
-    it 'is pending'
+    it 'returns magento object from hash' do
+      class Magento::SameClass; end
+      hash = { name: 'Some name', price: 10.99 }
+
+      object = Magento::ModelMapper.map_hash(Magento::SameClass, hash)
+
+      expect(object).to be_instance_of(Magento::SameClass)
+      expect(object.name).to eql hash[:name]
+      expect(object.price).to eql hash[:price]
+    end
   end
 
   describe '.map_array' do
-    it 'is pending'
+    it 'returns magento object list from array of hash' do
+      class Magento::SameClass; end
+      array = [{ name: 'Some name', price: 10.99 }]
+
+      object = Magento::ModelMapper.map_array('same_class', array)
+
+      expect(object).to be_a(Array)
+      expect(object).to all be_instance_of(Magento::SameClass)
+    end
   end
 
-  describe 'ModelParser.build' do
-    it 'is pending'
-  end
+  describe 'include ModelParser' do
+    before do
+      class Magento::SameClass
+        include Magento::ModelParser
+      end
+    end
 
-  describe 'ModelParser#to_h' do
-    it 'is pending'
+    let(:hash) { { name: 'Same name' } }
+
+    describe '.build' do
+      it 'calls Magento::ModelMapper.map_hash' do
+        expect(Magento::ModelMapper).to receive(:map_hash)
+          .with(Magento::SameClass, hash)
+
+        Magento::SameClass.build(hash)
+      end
+    end
+
+    describe '#to_h' do
+      it 'calls Magento::ModelMapper.to_hash' do
+        object = Magento::SameClass.build(hash)
+
+        expect(Magento::ModelMapper).to receive(:to_hash).with(object)
+
+        object.to_h
+      end
+    end
   end
 end
