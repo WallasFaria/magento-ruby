3af77b9beb729ba03aa62c18eed805fa6908d8b9
Giancarlo Rocha <giancarloiff@gmail.com>
2021-05-05 10:37:36 -0300

    fix docstrings

--- lib/magento/cart.rb
+++ lib/magento/cart.rb
@@ -8,9 +8,10 @@ module Magento
     #
     # Add a coupon by code to the current cart.
     #
-    # Example
-    # cart = Magento::Cart.find(1)
-    # cart.add_coupon('COAU4HXE0I')
+    # Example:
+    #
+    #   cart = Magento::Cart.find(1)
+    #   cart.add_coupon('COAU4HXE0I')
     #
     # @return Boolean: true on success, false otherwise
     def add_coupon(coupon)
@@ -21,8 +22,9 @@ module Magento
     # Delete cart's coupon
     #
     # Example:
-    # cart = Magento::Cart.find(1)
-    # cart.delete_coupon()
+    #
+    #   cart = Magento::Cart.find(1)
+    #   cart.delete_coupon()
     #
     # @return Boolean: true on success, raise exception otherwise
     def delete_coupon
@@ -33,11 +35,12 @@ module Magento
       #
       # Add a coupon by code to a specified cart.
       #
-      # Example
-      # Magento::Cart.add_coupon(
-      #   1,
-      #   'COAU4HXE0I'
-      # )
+      # Example:
+      #
+      #   Magento::Cart.add_coupon(
+      #     1,
+      #     'COAU4HXE0I'
+      #   )
       #
       # @return Boolean: true on success, false otherwise
       def add_coupon(id, coupon)
@@ -49,7 +52,8 @@ module Magento
       # Delete a coupon from a specified cart.
       #
       # Example:
-      # Magento::Cart.delete_coupon(1)
+      #
+      #   Magento::Cart.delete_coupon(1)
       #
       # @return Boolean: true on success, raise exception otherwise
       def delete_coupon(id)
