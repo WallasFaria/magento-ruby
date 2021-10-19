8d9e10068064ddf08ea25b682ec4df553ec93c14
Sebastian Menhofer <sebastian.menhofer@gmail.com>
2021-10-19 17:05:13 +0200

    Allow search for tax rates

--- lib/magento/tax_rate.rb
+++ lib/magento/tax_rate.rb
@@ -4,5 +4,13 @@ module Magento
   class TaxRate < Model
     self.primary_key = :id
     self.endpoint = 'taxRates'
+
+    class << self
+      protected
+
+      def query
+        Query.new(self, api_resource: 'taxRates/search')
+      end
+    end
   end
 end
